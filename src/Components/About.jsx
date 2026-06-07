6c744f64628d4acd5381a065950608be3ac9d546
Dev-MHliton <liton41126nk@gmail.com>
2026-06-07 22:07:54 +0600

    profile image set

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,6 +3,8 @@ import profile from '../assets/Liton.jpeg'
 const About = () => (
     <section id="about" className='pb-16 lg:mb-12'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-16 w-auto lg:grid-cols-2 justify-between items-center px-8'>
+
+            {/* Profile image set  */}
             <div>
                 <img
                     src={profile}
@@ -30,6 +32,6 @@ const About = () => (
             </div>
         </div>
     </section>
-
 );
+
 export default About;
